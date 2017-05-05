0d4cec4cce853c1cd56a5c6d85eeda9d66c25c6b
Andrew Han <handrew11@gmail.com>
2017-05-04 21:28:21 -0700

    ordering from bottom up

--- all-weather.py
+++ all-weather.py
@@ -23,40 +23,13 @@ from modules.all_weather_settings import *
 # TIER_CHOICE
 # VOL_WINDOW
 
-def main():
-	pp = pprint.PrettyPrinter(indent=4)
-
-	# first get ticker price and volatility data
-	print ">> Getting ticker data..."
-	ticker_data = get_ticker_data() 
-
-	ticker_volatilities = get_ticker_volatilities(ticker_data)
-	# then treat each group (like stocks) as its own portfolio and equalize volatility contributions
-	asset_class_weights = get_asset_class_weights(ticker_volatilities)
-	# then treat each box as its own portfolio and equalize volatility contributions
-	box_weights = get_box_weights(ticker_volatilities, asset_class_weights) 
-	# then treat each box as an asset itself in a four-asset portfolio and equalize contributions
-	environment_weights = get_environment_weights(ticker_volatilities, asset_class_weights, box_weights) 
-	# find individual asset weight by multiplying box_weights and environment_weights per my all weather configuration
-	weight_dict = finalize_ticker_weights(asset_class_weights, environment_weights, box_weights)
-
-	print "\n>> Volatilities"
-	pp.pprint(ticker_volatilities)
-	print "\n>> Box weights"
-	pp.pprint(box_weights)
-	print "\n>> Environment weights"
-	pp.pprint(environment_weights)
-	print "\n>> Final weights"
-	pp.pprint(weight_dict)
-
-	update_weight_file(weight_dict)
-	backtesting.backtest(weight_dict, output=True) # yes, this is backtesting with weights we could have only known today, so it's not super rigorous
 
 def update_weight_file(weight_dict):
 	weights = pd.read_csv(WEIGHTS_FILE).T.to_dict().values()
 	weights.append(weight_dict)
 	weights = pd.DataFrame(weights)
 	weights.to_csv(WEIGHTS_FILE, index=False)
+
 
 # given a tuple of (label, value)s, return
 # risk parity equalized {"label": weight}
@@ -85,21 +58,6 @@ def equalize_weights(tuples):
 	else:
 		return collections.defaultdict(lambda: 0.0)
 
-def get_ticker_volatilities(ticker_data):
-	ticker_volatilities = {}
-	for ticker in ticker_data:
-		ticker_volatilities[ticker] = util.get_annualized_variance_of_series(ticker_data[ticker]['Returns'], window=VOL_WINDOW)
-
-	ticker_volatilities = perform_variance_overrides(ticker_volatilities)
-	return ticker_volatilities
-
-def perform_variance_overrides(ticker_volatilities):
-	for ticker in TICKER_VOLATILITY_OVERRIDES:
-		if (ticker in ticker_volatilities): 
-			print ">> Overriding volatility %s. Setting to %0.05f" % (ticker, TICKER_VOLATILITY_OVERRIDES[ticker])
-			ticker_volatilities[ticker] = TICKER_VOLATILITY_OVERRIDES[ticker]
-
-	return ticker_volatilities
 
 # environment_box: gf|gr|ir|if
 def finalize_ticker_weights(asset_class_weights, environment_weights, box_weights):
@@ -129,29 +87,6 @@ def finalize_ticker_weights(asset_class_weights, environment_weights, box_weight
 
 	return weights_dict
 
-def get_ticker_data():
-	# get all ticker price data -- we take the window of volatility 
-	# in util.get_annualized_volatility_of_series
-	start = datetime.datetime(1940, 1, 1)
-	end = datetime.datetime.now()
-
-	ret = {}
-	for group in TICKERS:
-		for ticker in TICKERS[group]:
-			ret[ticker] = util.get_returns(ticker, start=start, end=end)
-
-	return ret
-
-# return {asset_class: {tickers: weights}}
-def get_asset_class_weights(ticker_volatilities):
-	asset_class_weights = {}
-	for asset_class in TICKERS: # stocks, commodities, EM credit, etc
-		tickers_in_asset_class = TICKERS[asset_class]
-		volatilities_for_tickers = [ticker_volatilities[ticker] for ticker in tickers_in_asset_class]
-		ordered_weights_by_ticker = equalize_weights(zip(tickers_in_asset_class, volatilities_for_tickers)).values()
-		asset_class_weights[asset_class] = dict(zip(tickers_in_asset_class, ordered_weights_by_ticker))
-
-	return asset_class_weights
 
 # return {"stocks": int, "commodities": int, etc} 
 # @param: whatever is returend from get_asset_class_weights
@@ -165,6 +100,35 @@ def get_asset_class_volatilities_from_ticker_weights(asset_class_weights, ticker
 			volatility += weight * ticker_volatilities[ticker]
 		asset_volatilities[asset_class] = volatility
 	return asset_volatilities
+
+
+def get_environment_weights(ticker_volatilities, weights_per_asset_class, weights_per_box):
+	asset_volatilities = get_asset_class_volatilities_from_ticker_weights(weights_per_asset_class, ticker_volatilities)
+	gr_vol = weights_per_box['gr']['stocks'] * asset_volatilities['stocks'] \
+			+ weights_per_box['gr']['commodities'] * asset_volatilities['commodities'] \
+			+ weights_per_box['gr']['EM credit'] * asset_volatilities['EM credit'] \
+			+ weights_per_box['gr']['corporate credit'] * asset_volatilities['corporate credit']
+	gf_vol = weights_per_box['gf']['nominal bonds'] * asset_volatilities['nominal bonds'] \
+			+ weights_per_box['gf']["inflation-linked"] * asset_volatilities["inflation-linked"]
+	ir_vol = weights_per_box['ir']["inflation-linked"] * asset_volatilities["inflation-linked"] \
+			+ weights_per_box['ir']['commodities'] * asset_volatilities['commodities'] \
+			+ weights_per_box['ir']['EM credit'] * asset_volatilities['EM credit']
+	if_vol = weights_per_box['if']['stocks'] * asset_volatilities['stocks'] \
+			+ weights_per_box['if']['nominal bonds'] * asset_volatilities['nominal bonds']
+
+	environment_weights = equalize_weights([('gr', gr_vol), ('gf', gf_vol), ('ir', ir_vol), ('if', if_vol)])
+	gr_weight = environment_weights['gr']
+	gf_weight = environment_weights['gf']
+	ir_weight = environment_weights['ir']
+	if_weight = environment_weights['if']
+
+	return { 
+		"gr": gr_weight,
+		"gf": gf_weight,
+		"ir": ir_weight,
+		"if": if_weight
+	}
+
 
 def get_box_weights(ticker_volatilities, asset_class_weights):
 	asset_volatilities = get_asset_class_volatilities_from_ticker_weights(asset_class_weights, ticker_volatilities)
@@ -203,32 +167,80 @@ def get_box_weights(ticker_volatilities, asset_class_weights):
 		"if": {"nominal bonds": nominal_bonds_weight_if, "stocks": stocks_weight_if},
 	}
 
-def get_environment_weights(ticker_volatilities, weights_per_asset_class, weights_per_box):
-	asset_volatilities = get_asset_class_volatilities_from_ticker_weights(weights_per_asset_class, ticker_volatilities)
-	gr_vol = weights_per_box['gr']['stocks'] * asset_volatilities['stocks'] \
-			+ weights_per_box['gr']['commodities'] * asset_volatilities['commodities'] \
-			+ weights_per_box['gr']['EM credit'] * asset_volatilities['EM credit'] \
-			+ weights_per_box['gr']['corporate credit'] * asset_volatilities['corporate credit']
-	gf_vol = weights_per_box['gf']['nominal bonds'] * asset_volatilities['nominal bonds'] \
-			+ weights_per_box['gf']["inflation-linked"] * asset_volatilities["inflation-linked"]
-	ir_vol = weights_per_box['ir']["inflation-linked"] * asset_volatilities["inflation-linked"] \
-			+ weights_per_box['ir']['commodities'] * asset_volatilities['commodities'] \
-			+ weights_per_box['ir']['EM credit'] * asset_volatilities['EM credit']
-	if_vol = weights_per_box['if']['stocks'] * asset_volatilities['stocks'] \
-			+ weights_per_box['if']['nominal bonds'] * asset_volatilities['nominal bonds']
 
-	environment_weights = equalize_weights([('gr', gr_vol), ('gf', gf_vol), ('ir', ir_vol), ('if', if_vol)])
-	gr_weight = environment_weights['gr']
-	gf_weight = environment_weights['gf']
-	ir_weight = environment_weights['ir']
-	if_weight = environment_weights['if']
+# return {asset_class: {tickers: weights}}
+def get_asset_class_weights(ticker_volatilities):
+	asset_class_weights = {}
+	for asset_class in TICKERS: # stocks, commodities, EM credit, etc
+		tickers_in_asset_class = TICKERS[asset_class]
+		volatilities_for_tickers = [ticker_volatilities[ticker] for ticker in tickers_in_asset_class]
+		ordered_weights_by_ticker = equalize_weights(zip(tickers_in_asset_class, volatilities_for_tickers)).values()
+		asset_class_weights[asset_class] = dict(zip(tickers_in_asset_class, ordered_weights_by_ticker))
 
-	return { 
-		"gr": gr_weight,
-		"gf": gf_weight,
-		"ir": ir_weight,
-		"if": if_weight
-	}
+	return asset_class_weights
+
+
+# Overriding historical volatility with implied
+def perform_variance_overrides(ticker_volatilities):
+	for ticker in TICKER_VOLATILITY_OVERRIDES:
+		if (ticker in ticker_volatilities): 
+			print ">> Overriding volatility %s. Setting to %0.05f" % (ticker, TICKER_VOLATILITY_OVERRIDES[ticker])
+			ticker_volatilities[ticker] = TICKER_VOLATILITY_OVERRIDES[ticker]
+
+	return ticker_volatilities
+
+
+# given a dictionary from get_ticker_data, get volatility
+def get_ticker_volatilities(ticker_data):
+	ticker_volatilities = {}
+	for ticker in ticker_data:
+		ticker_volatilities[ticker] = util.get_annualized_variance_of_series(ticker_data[ticker]['Returns'], window=VOL_WINDOW)
+
+	ticker_volatilities = perform_variance_overrides(ticker_volatilities)
+	return ticker_volatilities
+
+
+# get price history
+def get_ticker_data(start=datetime.datetime(1940, 1, 1), end = datetime.datetime.now()):
+	# get all ticker price data -- we take the window of volatility 
+	# in util.get_annualized_volatility_of_series
+	ret = {}
+	for group in TICKERS:
+		for ticker in TICKERS[group]:
+			ret[ticker] = util.get_returns(ticker, start=start, end=end)
+
+	return ret
+
+
+def main():
+	pp = pprint.PrettyPrinter(indent=4)
+
+	# first get ticker price and volatility data
+	print ">> Getting ticker data..."
+	ticker_data = get_ticker_data() 
+
+	ticker_volatilities = get_ticker_volatilities(ticker_data)
+	pdb.set_trace()
+	# then treat each group (like stocks) as its own portfolio and equalize volatility contributions
+	asset_class_weights = get_asset_class_weights(ticker_volatilities)
+	# then treat each box as its own portfolio and equalize volatility contributions
+	box_weights = get_box_weights(ticker_volatilities, asset_class_weights) 
+	# then treat each box as an asset itself in a four-asset portfolio and equalize contributions
+	environment_weights = get_environment_weights(ticker_volatilities, asset_class_weights, box_weights) 
+	# find individual asset weight by multiplying box_weights and environment_weights per my all weather configuration
+	weight_dict = finalize_ticker_weights(asset_class_weights, environment_weights, box_weights)
+
+	print "\n>> Volatilities"
+	pp.pprint(ticker_volatilities)
+	print "\n>> Box weights"
+	pp.pprint(box_weights)
+	print "\n>> Environment weights"
+	pp.pprint(environment_weights)
+	print "\n>> Final weights"
+	pp.pprint(weight_dict)
+
+	update_weight_file(weight_dict)
+	backtesting.backtest(weight_dict, output=True) # yes, this is backtesting with weights we could have only known today, so it's not super rigorous
 
 
 if __name__ == "__main__":
